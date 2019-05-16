c8ac5a74c984b22cf05792403f008f2917008377
Виктор Чернышов <192.168.127.1@mail.ru>
2019-05-16 08:43:50 +0300

    Создал первое приложение ASP.net Core MVC

---
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван" ,
+                SurName = "Иванов" ,
+                Patronymic = "Иванович" ,
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав" ,
+                SurName = "Петров" ,
+                Patronymic = "Иванович" ,
+                Age = 35
+            }
+        };
+
+        public IActionResult Index()
+        {
+            //return Content("Hello from controller");
+            return View(_employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_employees.Find(x => x.Id == id));
+        }
+    }
+}
---
+++ WebStore/WebStore/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Добавляем свойство для доступа к конфигурации
+        /// </summary>
+        public IConfiguration Configuration { get; }
+        /// <summary>
+        /// Добавляем новый конструктор, принимающий интерфейс IConfiguration
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Добавляем сервисы, необходимые для mvc
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Добавляем расширение для использования статических файлов, т.к.
+            //appsettings.json - это статический файл
+            app.UseStaticFiles();
+            //var hello = Configuration["CustomHelloWorld"];
+
+            //Добавляем обработку запросов в mvc-формате
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            /*
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(hello);
+            });
+            */
+        }
+    }
+}
