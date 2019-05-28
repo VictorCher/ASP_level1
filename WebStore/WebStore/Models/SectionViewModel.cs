0d73a9b43cf598a8222beb88915a199f5e2ca339
VictorCher <192.168.127.1@mail.ru>
2019-05-28 22:20:04 +0300

    Почти все сделал, остались только фильтры

---
+++ WebStore/WebStore/Models/SectionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Models
+{
+    public class SectionViewModel : INamedEntity, IOrderedEntity
+    {
+        public SectionViewModel()
+        {
+            ChildSections = new List<SectionViewModel>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        /// <summary>
+        /// Дочерние секции
+        /// </summary>
+        public List<SectionViewModel> ChildSections { get; set; }
+        /// <summary>
+        /// Родительская секция
+        /// </summary>
+        public SectionViewModel ParentSection { get; set; }
+    }
+}
