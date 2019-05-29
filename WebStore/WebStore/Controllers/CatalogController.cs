e441917a729a4c8126fef2d5667d4e50763eed6c
VictorCher <192.168.127.1@mail.ru>
2019-05-29 23:31:30 +0300

    Доделал, но ничего не работает

--- WebStore/WebStore/Controllers/CatalogController.cs
+++ WebStore/WebStore/Controllers/CatalogController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewComponents;
+using WebStore.Domain.Entities.Filters;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+using WebStore.Infrastructure.Implementations;
 
 namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
-        public IActionResult Shop()
+        private readonly IProductData _productData;
+        
+        public IActionResult Shop(int? sectionId, int? brandId)
         {
-            return View();
+            var products = _productData.GetProducts(new ProductFilter
+            {
+                BrandId = brandId,
+                SectionId = sectionId
+            });
+            var model = new CatalogViewModel()
+            {
+                BrandId = brandId,
+                SectionId = sectionId,
+                Products = products.Select(p => new ProductViewModel()
+                {
+                    Id = p.Id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price
+                }).OrderBy(p => p.Order).ToList()
+            };
+            return View(model);
         }
 
         public IActionResult ProductDetails()
