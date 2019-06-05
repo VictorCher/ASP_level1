4235b930d13489c4fc8d2564fa6f32ea7437d54e
Виктор Чернышов <192.168.127.1@mail.ru>
2019-06-05 17:14:53 +0300

    Вроде все сделал

--- WebStore/WebStore/Models/Account/LoginViewModel.cs
+++ WebStore/WebStore/Models/Account/LoginViewModel.cs
@@ -7,7 +7,7 @@ using WebStore.DomainNew.Entities.Base.Interfaces;
 
 namespace WebStore.Models.Account
 {
-    public class RegisterUserViewModel
+    public class LoginViewModel
     {
         [Required, MaxLength(256)]
         public string UserName { get; set; }
@@ -15,10 +15,12 @@ namespace WebStore.Models.Account
         [Required, DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [DataType(DataType.Password), Compare(nameof(Password))]
-        public string ConfirmPassword { get; set; }
-
         [Required, DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Запомнить на сайте")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
     }
 }
